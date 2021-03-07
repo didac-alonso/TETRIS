252f04464838c08ef2855ce6b06a444e1f3bbc96
Didac <didac.emdn@gmail.com>
2021-03-07 21:55:37 +0100

    Yupi

--- gameboard.py
+++ gameboard.py
@@ -5,8 +5,8 @@ Location = collections.namedtuple('Location', 'row column')
 
 class GameBoard:
     #constructor
-    Shape = collections.namedtuple('Shape', 'width height')
-    Location = collections.namedtuple('Location', 'row column')
+    # Shape = collections.namedtuple('Shape', 'width height')
+    # Location = collections.namedtuple('Location', 'row column')
 
     def __init__(self, tamany):
         self.height = tamany.height
