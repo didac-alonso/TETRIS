694ecec4168830573ded280dc2e40f6897ff4125
Didac <didac.emdn@gmail.com>
2021-03-18 10:08:53 +0100

    Commentaries + assertation

--- gameboard.py
+++ gameboard.py
@@ -6,10 +6,12 @@ Location = collections.namedtuple('Location', 'row column')
 class GameBoard:
 
     def __init__(self, size):
-    """
-    Constructor, gameboard is based in 3 parameters two integers: 
-    height, width and a matrix: board which represents the gameboard
-    """
+        """
+        Constructor: it needs a Shape(width, height) gameboard is based in 3 parameters, two integers: 
+        height, width and a matrix: board which represents the gameboard.
+        
+        Precondition: height and widht > 0
+        """
         self.height = size.height
         self.width = size.width
         matriu =[]
@@ -21,10 +23,11 @@ class GameBoard:
         self.board = matriu
 
 
+    # It makes it in O(w), where w is the board width. I use a vector which is a complete row,
+    # to compare directly to the entire rows.
     def full_rows(self):
         """ 
-        This method returns a list which contains the full rows are the full rows.
-        It makes it in O(w), where w is the board width
+        This method returns a list which contains the full rows.
         """
         size = self.get_shape()
         rows = []
@@ -35,11 +38,12 @@ class GameBoard:
         return rows
 
 
+    # It makes it in O(h), where h is the board height, to do so, 
+    # I have to transpose the board in order to get
+    # its rows, that's because it isn't possible to acces by columns in a matrix. 
     def full_columns(self):
         """
-        This method returns a list which contains the full columns. It makes it in O(h),
-        where h is the board height, to do so, i have to transpose the board in order to get
-        its rows, thats because it isn't possible to acces by columns in a matrix 
+        This method returns a list which contains the full columns. 
         """
         size = self.get_shape()
         columns = []
@@ -50,8 +54,13 @@ class GameBoard:
                 columns.append(j) 
         return columns
 
+    # Private method, doesn't transform the coordinates, it's used by 
+    # a lot of methods which have transformed previously the coordinates.
     def has_token(self, location):
-    """This method returns whether a square has a token"""
+        """
+        This method returns whether a square has a token
+        Prec: the location is in the gameboard.
+        """
         return self.board[location.row][location.column] == '\u2b1b'
 
 
@@ -63,8 +72,13 @@ class GameBoard:
     #                 return False    
     #     return True
 
+    # It searches square by square all the tokens, then it transforms the location,
+    # and adds to a list which contains all the squares with token.
     def search_tokens(self):
-        """This method returns a list which contains all the squares with token"""
+        """
+        This method returns a list which contains the 
+        location of all the squares with token
+        """
         tokens_location = []
         size = self.get_shape()
         for i in range (size.height):
@@ -74,61 +88,63 @@ class GameBoard:
                     tokens_location.append(a)
         return tokens_location
 
+
+    # It uses the remove method in a entire row to erase it, using a for for moving beetwen the given rows,
+    # it goes row by row removing without checking if they are full a rectangle with row-shape. 
     def clear_rows(self, rows):
-    """ 
-    This method erases the tokens of the given rows, rows is a vector which contains
-    the rows which are going to be cleared
-    """
+        """ 
+        This method erases the tokens of the given rows, rows is a vector which contains
+        the rows which are going to be cleared.
+
+        Prec: The rows given are in the gameboard.
+        """
         size = self.get_shape()
-        # if row > size.height:
-        #     print("The row is out of bounds")
-        # else:
         number_of_rows = len(rows)
         for i in range (number_of_rows):
-            for j in range(size.width):
-                self.remove(Location(rows[i], j))
+            self.remove(Location(rows[i], 0),Shape(size.width, 1), False)
         return self
 
-
+    # It uses the remove method in a entire column to erase it, using a for for moving beetwen the given columns,
+    # it goes column by column removing without checking if they are full a rectangle with column-shape.
     def clear_columns(self, columns):
         """ 
         This method erases the tokens of the given columns, columns 
-        is a vector which contains the rows which are going to be cleared
+        is a vector which contains the rows which are going to be cleared.
+
+        Prec: The rows given are in the gameboard.
         """
         size = self.get_shape()
-        # if column > size.height:
-        #     print("The row is out of bounds")
-        # else:
         number_of_columns = len(columns)
         for j in range(number_of_columns):
-            for i in range (size.height):
-                self.remove(Location(i, columns[j]))
+            self.remove(Location(0, columns[j]), Shape(1, size.height), False)
         return self
 
 
     def out_bounds(self, location,size):
-    """This methods returns whether a location is out of the gameboard"""
+        """This methods returns whether a location is out of the gameboard"""
         return location.row >= size.height or location.row < 0 or location.column < 0 or location.column >= size.width
 
     def get_shape(self):
-    """This method returns the shape of the gameboard"""
+        """This method returns the shape of the gameboard"""
         width = self.width  
         h = self.height
         mesura = Shape(width, h)
         return mesura
 
+
     def transform_coordinates(self, location, size):
-    """ 
-    This private method, transform the coordinates of a given location
-    to the coordinates that are being used to identify the rows and columns
-    most methods use this one. 
-    """
+        """ 
+        This private method, transform the coordinates of a given location
+        to the coordinates that are being used to identify the rows and columns
+        most methods use this one. 
+        """
         return Location(size.height-location.row - 1,location.column)    
 
+
     def __str__(self):
-    """
-    This method defines how to print the gameboard, it prints the matrix.
-    """
+        """
+        This method defines how to print the gameboard, it prints the matrix.
+        """
         size = self.get_shape()
         for i in range(size.height):
             for j in range(size.width):
@@ -173,52 +189,64 @@ class GameBoard:
     #                 return False
     #     return True
 
+
+    # First of all it checks if the square is empty and it fits in the location given. If it's true, places the rectangle and fills the squares. 
     def put(self, location, rectangle = Shape(1, 1)):
-    """ 
-    This method puts, if it's possible, a rectangle of tokens, which has its low-left corner in location
-    if the rectangle is not specified, the method will place only one token, in the location 
-    """
+        """ 
+        This method puts, if it's possible, a rectangle of tokens, which has its low-left corner in location
+        if the rectangle is not specified, the method will place only one token, in the location.
+        """
+        try:
+            empty = self.is_empty(location, rectangle)   
+            assert empty, 'The square is already occupied.'
+        except:
+            assert 1 == 0, 'The square is out of bounds.'
+        # assert self.is_empty(location, rectangle)
         size = self.get_shape()
-        if not self.is_empty(location, rectangle):
-            print("The rectangle cannot be put on the board.")         
-            return self
+        # if not self.is_empty(location, rectangle):
+        #     print("The rectangle cannot be put on the board.")         
+        #     return self
         location = self.transform_coordinates(location, size)
         for i in range(rectangle.height):
             for j in range(rectangle.width):    
                 # if self.out_bounds(Location(location.row - i, location.column + j),size):
                 #     print("The square is out of bounds.")
                 #     return self
-                if self.has_token(Location(location.row - i, location.column + j)):
-                    print("The square is already occupied.")
-                    return self
-                else:
-                    self.board[location.row - i][location.column + j] = '\u2b1b'
+                # if self.has_token(Location(location.row - i, location.column + j)):
+                # #     print("The square is already occupied.")
+                #     return self
+                # else:
+                self.board[location.row - i][location.column + j] = '\u2b1b'
         return self
 
 
-    def remove(self, location,rectangle = Shape(1, 1)):
-    """ 
-    This method erases, if it's possible; that is, if the rectangle is fully occuped and it fits in the board,
-    a rectangle of tokens, which has its low-left corner in location
-    if the rectangle is not specified, the method will erase only one token, in the location.
-    """
+    # First of all it checks if the square is full and fits in the given location, then it erases the squares in the rectangle.
+    # It has an extra parametre that default is true, that is because for clear_rows() and clear_columns doesn't matter if they are
+    # full or not.
+    def remove(self, location,rectangle = Shape(1, 1), must_empty = True):
+        """ 
+        This method erases, if it's possible; that is, if the rectangle is fully occuped and it fits in the board,
+        a rectangle of tokens, which has its low-left corner in location
+        if the rectangle is not specified, the method will erase only one token, in the location.
+        """
         size = self.get_shape()
-        if not self.is_full(location, rectangle):
-            print("The rectangle cannot be removed.")         
-            return self
+        if must_empty:
+            if not self.is_full(location, rectangle):
+                print("The rectangle cannot be removed.")         
+                return self
         location = self.transform_coordinates(location, size)
         for i in range(rectangle.height):
             for j in range (rectangle.width):
                 self.board[location.row - i][location.column + j] = '\u2b1c'
         return self
 
-
+    # This method returns false if the rectangle is not empty, and it asserts if it's out of bounds.
     def is_empty(self,location, rectangle=Shape(1,1)):
-    """ 
-    This method returns wether a rectangle is empty, the rectangle is specified by its shape
-    and location is the low-left corner. If the rectangle is not specified, it considers
-    only the location of low-left corner.
-    """
+        """ 
+        This method returns wether a rectangle is empty, the rectangle is specified by its shape
+        and location is the low-left corner. If the rectangle is not specified, it considers
+        only the location of low-left corner.
+        """
         size = self.get_shape()
         location = self.transform_coordinates(location, size)
         for i in range(rectangle.height):
@@ -227,16 +255,16 @@ class GameBoard:
                     if self.has_token(Location(location.row - i, location.column + j)):
                         return False
                 except:
-                    return False
+                    assert 1 == 0, 'The square is out of bounds.'
         return True
 
-
+    # This method returns false if the rectangle is not fully occuped, and it asserts if it's out of bounds.
     def is_full(self,location, rectangle=Shape(1,1)):
-    """
-    This method returns wether a rectangle is full, the rectangle is specified by its shape
-    and location is the low-left corner. If the rectangle is not specified, it considers
-    only the location of low-left corner
-    """
+        """
+        This method returns wether a rectangle is full, the rectangle is specified by its shape
+        and location is the low-left corner. If the rectangle is not specified, it considers
+        only the location of low-left corner
+        """
         size = self.get_shape()
         location = self.transform_coordinates(location, size)
         for i in range(rectangle.height):
@@ -245,14 +273,16 @@ class GameBoard:
                     if not self.has_token(Location(location.row - i, location.column + j)):
                         return False
                 except:
-                    return False
+                    assert 1 == 0, 'The square is out of bounds.'
         return True
 
+    # It changes the data shown by the type gameboard, it must be shown the shape, and squares with token,
+    # it uses the search_tokens function in order to find the tokens, then it prints it.
     def __repr__(self):
-    """
-    This method changes de data shown by the type GameBoard, the data that must be shown is,
-    the shape, and the squares with token
-    """
+        """
+        This method changes the data shown by the type GameBoard, the data that must be shown is,
+        the shape, and the squares with token
+        """
         size = self.get_shape()
         print(size.width,end='')
         print("x", end = '')
@@ -263,10 +293,12 @@ class GameBoard:
         return ""
 
 
+    # It goes row by row counting how many occupied square are, using two fors.
+    # The for starts from size.height -1 in order to return the counter from the 0 row to height - 1 row.
     def row_counters(self):
-    """
-    This method counts how many tokens are in every row, it returns it in a list.
-    """
+        """
+        This method counts how many tokens are in every row, it returns it in a list.
+        """
         size = self.get_shape()
         row_counter = []        
         for i in range(size.height-1, -1, -1):
@@ -277,10 +309,11 @@ class GameBoard:
             row_counter.append(counter)
         return row_counter
 
+    # It goes column by column counting how many occupied square are in each one.
     def column_counters(self):
-    """
-    This method counts how many tokens are in every column, it returns it in a list.
-    """
+        """
+        This method counts how many tokens are in every column, it returns it in a list.
+        """
         size = self.get_shape()
         column_counter = []        
         for j in range(size.width):
--- myplayer.py
+++ myplayer.py
@@ -2,22 +2,28 @@ from gameboard import *
 
 class MyPlayer:
 
+
+    # Constructor, it uses the gameboard class to use all its methods, and a string which contains the skill of the bot.
     def __init__(self ,width, height, method = "simple"):
         """This is the constructor, it, creates a gameboard, which needs a Shape, and you can define wether is
             an expert AI or the simple one."""
         self.board = GameBoard(Shape(width,height))
         self.method = method
     
+    # It uses the put, method in order to use all the methods of the GameBoard class.
     def place_block(self, location, block):
         """This method puts a block if it's possible in a given location, it uses the put method from gameboard"""
-        self.board.put(location,block).board
-        #print("The block could not be placed. The board has not been modified")
+        try:
+            self.board.put(location,block).board
+        except:
+            assert 1 == 0, 'The block could not be placed'
         full_row = self.board.full_rows()
         full_column = self.board.full_columns()
         self.board.clear_columns(full_column)
         self.board.clear_rows(full_row)    
         return self
         
+    # It uses the print method from the GameBoard class.
     def __str__(self):
         """
         This method uses the __str__ method of gameboard.
@@ -25,6 +31,9 @@ class MyPlayer:
         print(self.board)
         return ""
 
+    # This method have the instructions which the AI follows, the simple places the block as bottom-left is possible, and the
+    # expert does the same, but if puting one block it completes a row or column, it takes that location, to do so I try putting
+    # the block in the location and if it makes a column or row be full it returns that location.
     def play(self, block):
         """
         This method defines playing method, the simple one places the block as bottom-left is possible. 
@@ -33,26 +42,34 @@ class MyPlayer:
         size = self.board.get_shape()
         if self.method == "simple":
             for i in range (size.height):
-                for j in range(size.width):    
-                    if self.board.is_empty(Location(i,j), block):
-                        return Location(i, j)
+                for j in range(size.width):
+                    try:    
+                        if self.board.is_empty(Location(i,j), block):
+                            return Location(i, j)
+                    except:
+                        pass
             return None
+        
         else:
             provisional_loc = None
             for i in range(size.height):
                 for j in range(size.width):
-                    if self.board.is_empty(Location(i,j),block):
-                        if provisional_loc == None:
-                            provisional_loc = Location(i,j)
-                        self.board.put(Location(i, j), block)
-                        if self.board.full_columns() != [] or self.board.full_rows() != []:
+                    try:
+                        if self.board.is_empty(Location(i,j),block):
+                            if provisional_loc == None:
+                                provisional_loc = Location(i,j)
+                            self.board.put(Location(i, j), block)
+                            if self.board.full_columns() != [] or self.board.full_rows() != []:
+                                self.board.remove(Location(i,j), block)
+                                return Location(i,j)
                             self.board.remove(Location(i,j), block)
-                            return Location(i,j)
-                        self.board.remove(Location(i,j), block)
+                    except:
+                        pass
             return provisional_loc
             
                     
-    
+    # This is a control method, it's used to indicate that the board is smaller than the block that
+    # is trying to place
     def is_legal(self, block):
         """
         This method returns a boolean which is true if
